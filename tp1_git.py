92cb21da3cb9952b1c96e1626e12bbebc8a2617e
JuanCruz <jcizaurralde@gmail.com>
2020-09-05 15:25:22 -0300

    La consigna del trabajo

---
+++ tp1_git.py
@@ -0,0 +1,5 @@
+# Para tributar un determinado impuesto se debe ser mayor de 16 años 
+# y tener unos ingresos iguales o superiores a $ 90000 mensuales. 
+# Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales
+# y muestre por pantalla si el usuario tiene que tributar o no.
+
